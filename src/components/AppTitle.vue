1dcc2fbb275e99384e9bfcfa73215f7c552bacf9
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-10 15:56:36 +0200

    feat: add title component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        name: 'AppTitle'
+    }
+
+    
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style>
+h1{
+    text-align: center
+}
+</style>
